f3ceab19eff46b7ab53d0b1a9ada80b6423ca3c5
HugoHenry <quanghieusieubeo@gmail.com>
2019-12-10 23:39:39 +0700

    new

---
+++ config/keys.js
@@ -0,0 +1,15 @@
+// add this file to .gitignore
+
+module.exports = {
+    google: {
+        clientID: '917662851597-kal4i1mtutj3ggriutt65rpeje1jbjsi.apps.googleusercontent.com',
+        clientSecret: 'RzK4YqnSdE8RTHW5toWdSIaX'
+    },
+    mongodb:{
+        dbURL:'mongodb://admin:CodesePre02@codedidungso.me:27017/codese-pre-02'
+        
+    },
+    session:{
+        cookieKey:'HieuthichHa'
+    }
+}
